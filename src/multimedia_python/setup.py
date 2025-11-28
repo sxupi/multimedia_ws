3ccc1f4edd8542a4ff978bff724598207bbae5ec
Filip Scopulovic <f.scopulovic@gmail.com>
2025-11-28 20:58:01 +0100

    Initalized the whole multimedia station project

---
+++ src/multimedia_python/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'multimedia_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='filip scopulovic',
+    maintainer_email='f.scopulovic@gmail.com',
+    description='This packages contains all Python-based nodes for the multimedia station acting as a radio station and Spotify client.',
+    license='GPL-3.0-only',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

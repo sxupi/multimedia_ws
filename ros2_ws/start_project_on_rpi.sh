c28ff7c959045c4ed4dcb4c21ed90690967a925a
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 17:51:34 +0100

    Reworked start script so that it uses launch

--- ros2_ws/start_project_on_rpi.sh
+++ ros2_ws/start_project_on_rpi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /opt/ros/jazzy/setup.bash
 source /home/ubuntu/multimedia_ws/ros2_ws/install/setup.bash
-ros2 run multimedia_python start_multimedia
+ros2 launch multimedia_python multimedia_system.launch.py

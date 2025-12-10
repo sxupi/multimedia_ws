d31a1c1322d70227a13a9ebef04bf31a8dab2766
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 11:57:26 +0100

    Added small version of the LCD Display Node

---
+++ ros2_ws/src/multimedia_python/multimedia_python/lcd_display_node.py
@@ -0,0 +1,20 @@
+from rclpy.node import Node
+from std_msgs.msg import Float32, String
+
+from .external.LCD import LCD
+
+class LCDDisplayNode(Node):
+
+    def __init__(self):
+        super().__init__('volume_controller')
+        self.first_line_subscriber_ = self.create_subscription(
+            String,
+            '/lcd_display/first_string',
+            self.__first_line_callback,
+            10
+        )
+
+#        self._lcd = LCD(i2c_addr=39)
+
+    def __first_line_callback(self, msg: String) -> None:
+        self.get_logger().info(f'Received first line {msg.data}')

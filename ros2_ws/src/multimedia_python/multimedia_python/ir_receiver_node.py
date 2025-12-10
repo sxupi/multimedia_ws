12a99162f012fd0be8199404a5bf632e516209b2
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 10:02:20 +0100

    New codes

--- ros2_ws/src/multimedia_python/multimedia_python/ir_receiver_node.py
+++ ros2_ws/src/multimedia_python/multimedia_python/ir_receiver_node.py
@@ -8,12 +8,12 @@ from .external import IRModule
 class IRReceiverNode(Node):
 
     COMMAND_MAPPINGS = {
-        0x300ffa857: 'PLAY_STOP',
-        0x300ff22dd: 'SWITCH',
-        0x300ff02fd: 'HIGHER_VOLUME',
-        0x300ff9867: 'LOWER_VOLUME',
-        0x300ff906f: 'NEXT',
-        0x300ffe01f: 'PREV'
+        16754775: 'PLAY_STOP',  # 0x300ffa857: 'PLAY_STOP',
+        16720605: 'SWITCH',  # 0x300ff22dd: 'SWITCH',
+        16712445: 'HIGHER_VOLUME',  # 0x300ff02fd: 'HIGHER_VOLUME',
+        16750695: 'LOWER_VOLUME',  # 0x300ff9867: 'LOWER_VOLUME',
+        16748655: 'NEXT',  # 0x300ff906f: 'NEXT',
+        16769055: 'PREV'  # 0x300ffe01f: 'PREV'
     }
 
     def __init__(self, bcm_num=23):
@@ -29,16 +29,19 @@ class IRReceiverNode(Node):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(bcm_num, GPIO.IN)
-        GPIO.add_event_detect(bcm_num, GPIO.BOTH, callback=self.__ir_module.pWidth)
+        GPIO.add_event_detect(bcm_num, GPIO.BOTH,
+                              callback=self.__ir_module.pWidth)
 
         # No need to print out high and low durations
         self.__ir_module.set_verbose(False)
 
-        self.get_logger().info('Initialized IR receiver node with commands: {0}'.format(self.COMMAND_MAPPINGS))
+        self.get_logger().info(
+            'Initialized IR receiver node with commands: {0}'.format(self.COMMAND_MAPPINGS))
 
     def __ir_received(self, code) -> None:
         msg = String()
-        self.get_logger().info('Command: {0} ({1}) from {2}'.format(self.COMMAND_MAPPINGS.get(code), code, self.COMMAND_MAPPINGS))
-        #msg.data = self.COMMAND_MAPPINGS[code]
-        #self.__command_publisher_.publish(msg)
-        #self.get_logger().info('Publishing received command: {0} ({1})'.format(msg.data, code))
+        self.get_logger().info('Command: {0} ({1}) from {2}'.format(
+            self.COMMAND_MAPPINGS.get(code), code, self.COMMAND_MAPPINGS))
+        # msg.data = self.COMMAND_MAPPINGS[code]
+        # self.__command_publisher_.publish(msg)
+        # self.get_logger().info('Publishing received command: {0} ({1})'.format(msg.data, code))

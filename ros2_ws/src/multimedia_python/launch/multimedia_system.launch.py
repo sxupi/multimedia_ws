bb34acad59a3648595e3045168cea8ec1698719b
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 17:50:53 +0100

    Added launch script

--- ros2_ws/src/multimedia_python/launch/multimedia_system.launch.py
+++ ros2_ws/src/multimedia_python/launch/multimedia_system.launch.py
@@ -4,5 +4,29 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='multimedia_python', executable='start_multimedia'),
+        Node(
+            package='multimedia_python',
+            executable='radio_node',
+            name='radio_node'
+        ),
+        Node(
+            package='multimedia_python',
+            executable='volume_controller_node',
+            name='volume_controller_node'
+        ),
+        Node(
+            package='multimedia_python',
+            executable='frequency_controller_node',
+            name='frequency_controller_node'
+        ),
+        Node(
+            package='multimedia_python',
+            executable='ir_receiver_node',
+            name='ir_receiver_node'
+        ),
+        Node(
+            package='multimedia_python',
+            executable='lcd_display_node',
+            name='lcd_display_node'
+        ),
     ])

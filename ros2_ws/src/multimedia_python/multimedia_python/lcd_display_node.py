735aac4fc14c55fd61b55ce5835d3bc9cb0f198c
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 10:42:04 +0100

    Implemented LCD display node

---
+++ ros2_ws/src/multimedia_python/multimedia_python/lcd_display_node.py
@@ -0,0 +1,41 @@
+from rclpy.node import Node
+from std_msgs.msg import String
+
+from .external.LCD import LCD
+
+class LCDDisplayNode(Node):
+
+    def __init__(self):
+        super().__init__('lcd_displayer')
+        self.command_subscriber_ = self.create_subscription(
+            String,
+            '/display/first_line_string',
+            self.__set_first_line_callback,
+            10
+        )
+        self.command_subscriber_ = self.create_subscription(
+            String,
+            '/display/second_line_string',
+            self.__set_second_line_callback,
+            10
+        )
+
+        self._lcd = LCD()
+
+        self._first_line = 'Hello,'
+        self._second_line = 'multimedia station started'
+        
+        self.__refresh_display()
+
+    def __set_first_line_callback(self, msg: String) -> None:
+        self._first_line = msg.data
+        self.__refresh_display()
+
+    def __set_second_line_callback(self, msg: String) -> None:
+        self._second_line = msg.data
+        self.__refresh_display()
+
+    def __refresh_display(self) -> None:
+        self._lcd.clear()
+        self._lcd.message(self._first_line, 1)
+        self._lcd.message(self._second_line, 2)

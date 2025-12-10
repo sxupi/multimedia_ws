fe0a81495ac5f5e638ec4c10b38879443ec26953
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 10:43:40 +0100

    Added LCD Display Node to the start

--- ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
+++ ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
@@ -4,6 +4,7 @@ from rclpy.executors import MultiThreadedExecutor
 from .volume_controller_node import VolumeControllerNode
 from .freq_controller_node import FrequencyControllerNode
 from .ir_receiver_node import IRReceiverNode
+from .lcd_display_node import LCDDisplayNode
 
 def main(args=None):
     rclpy.init(args=args)
@@ -11,11 +12,13 @@ def main(args=None):
     volume_controller_node = VolumeControllerNode()
     frequency_controller_node = FrequencyControllerNode()
     ir_receiver_node = IRReceiverNode()
+    lcd_display_node = LCDDisplayNode()
 
     executor = MultiThreadedExecutor()
     executor.add_node(volume_controller_node)
     executor.add_node(frequency_controller_node)
     executor.add_node(ir_receiver_node)
+    executor.add_node(lcd_display_node)
 
     print('Starting to spin the executor now')
     try:
@@ -26,6 +29,7 @@ def main(args=None):
         volume_controller_node.destroy_node()
         frequency_controller_node.destroy_node()
         ir_receiver_node.destroy_node()
+        lcd_display_node.destroy_node()
         rclpy.shutdown()
 
 if __name__ == '__main__':

854b1faf64ada1a354069058aaca82827dff4dab
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 07:21:56 +0100

    Added start script

---
+++ ros2_ws/start_project.sh
@@ -0,0 +1,5 @@
+# run_my_gpio_node.sh
+#!/usr/bin/env bash
+source /opt/ros/jazzy/setup.bash
+source /home/ubuntu/multimedia_ws/ros2_ws/install/setup.bash
+ros2 run multimedia_python start_multimedia

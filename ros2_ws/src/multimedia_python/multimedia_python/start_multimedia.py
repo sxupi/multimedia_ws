3f3404a858685e5bef101a4c78f5e79802df07f2
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 21:54:37 +0100

    Made it obsolete

--- ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
+++ ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
@@ -4,33 +4,34 @@ from rclpy.executors import MultiThreadedExecutor
 from .volume_controller_node import VolumeControllerNode
 from .freq_controller_node import FrequencyControllerNode
 from .ir_receiver_node import IRReceiverNode
-from .lcd_display_node import LCDDisplayNode
+from .display_node import DisplayNode
 
 def main(args=None):
-    rclpy.init(args=args)
+    print("Obsolete now, please use the ros2 launch command")
+#    rclpy.init(args=args)
 
-    volume_controller_node = VolumeControllerNode()
-    frequency_controller_node = FrequencyControllerNode()
-    lcd_display_node = LCDDisplayNode()
-    ir_receiver_node = IRReceiverNode()
+#    volume_controller_node = VolumeControllerNode()
+#    frequency_controller_node = FrequencyControllerNode()
+#    display_node = DisplayNode()
+#    ir_receiver_node = IRReceiverNode()
 
-    executor = MultiThreadedExecutor()
-    executor.add_node(volume_controller_node)
-    executor.add_node(frequency_controller_node)
-    executor.add_node(lcd_display_node)
-    executor.add_node(ir_receiver_node)
+#    executor = MultiThreadedExecutor()
+#    executor.add_node(volume_controller_node)
+#    executor.add_node(frequency_controller_node)
+#    executor.add_node(display_node)
+#    executor.add_node(ir_receiver_node)
 
-    print('Starting to spin the executor now')
-    try:
-        executor.spin()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        volume_controller_node.destroy_node()
-        frequency_controller_node.destroy_node()
-        lcd_display_node.destroy_node()
-        ir_receiver_node.destroy_node()
-        rclpy.shutdown()
+#    print('Starting to spin the executor now')
+#    try:
+#        executor.spin()
+#    except KeyboardInterrupt:
+#        pass
+#    finally:
+#        volume_controller_node.destroy_node()
+#        frequency_controller_node.destroy_node()
+#        display_node.destroy_node()
+#        ir_receiver_node.destroy_node()
+#        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()

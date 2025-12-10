3a380f260d7ec96a6591b968122493f90b9bbe8a
Filip Scopulovic <f.scopulovic@gmail.com>
2025-12-10 12:03:52 +0100

    Checking if there is a limit of subscriptions

--- ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
+++ ros2_ws/src/multimedia_python/multimedia_python/start_multimedia.py
@@ -15,10 +15,10 @@ def main(args=None):
     ir_receiver_node = IRReceiverNode()
 
     executor = MultiThreadedExecutor()
-    executor.add_node(volume_controller_node)
-    executor.add_node(frequency_controller_node)
+    #executor.add_node(volume_controller_node)
+    #executor.add_node(frequency_controller_node)
     executor.add_node(lcd_display_node)
-    executor.add_node(ir_receiver_node)
+    #executor.add_node(ir_receiver_node)
 
     print('Starting to spin the executor now')
     try:
